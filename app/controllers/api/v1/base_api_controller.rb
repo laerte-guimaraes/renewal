8a07367986e4660e8e0db7d0ffd3d0d07d859b60
laerte-guimaraes <laerte.guimaraes@vindi.com.br>
2020-04-29 23:42:33 -0300

    Insere estrutura base para API

---
+++ app/controllers/api/v1/base_api_controller.rb
@@ -0,0 +1,13 @@
+module API::V1
+  lass BaseAPIController < ApplicationControllerAPI
+    acts_as_token_authentication_handler_for User
+
+    before_action :require_authentication!
+
+    private
+
+    def require_authentication!
+      throw(:warden, scope: :user) unless current_user.presence
+    end
+  end
+end
