75a0253085166b89f0440ba6bcc1613caca0684d
laerte-guimaraes <laerte.guimaraes@vindi.com.br>
2020-05-13 19:38:12 -0300

    Adiciona auto-geração de chave API

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   belongs_to :contract
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
+
+  before_create :generate_authentication_token
+
+  def generate_authentication_token
+    authentication_token = SecureRandom.urlsafe_base64(32)
+  end
 end
