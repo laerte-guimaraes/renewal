5c5bf39569499be4c095da0a2a9d964fffb8d4f9
Laerte Guimarães <31661772+laerte-guimaraes@users.noreply.github.com>
2020-05-15 14:27:18 -0300

    Insere serializer para filtrar atributos na resposta via API (#17)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ end
 require 'codacy-coverage'
 Codacy::Reporter.start
 
+require 'webmock/rspec'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
