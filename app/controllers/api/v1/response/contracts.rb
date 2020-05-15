f2b714fef6bac2552ee2c678b7e1006067194111
laerte-guimaraes <laerte.guimaraes@vindi.com.br>
2020-05-15 12:34:17 -0300

    Ajusta class de helper da API

--- app/controllers/api/v1/response/contracts.rb
+++ app/controllers/api/v1/response/contracts.rb
@@ -1,19 +1,19 @@
 module Api::V1::Response
-  class Contracts
-    class << self
-      def full(objects)
-        objects.map do |object|
-          full_parameters.each_with_object({}) do |parameter, response|
-            response.merge!(parameter => object.send(parameter))
-          end
+  module Contracts
+    module_function
+
+    def full(objects)
+      objects.map do |object|
+        full_parameters.each_with_object({}) do |parameter, response|
+          response.merge!(parameter => object.send(parameter))
         end
       end
-
-      private
-
-      def full_parameters
-        %i[id name created_at users]
-      end
     end
+
+    def full_parameters
+      %i[id name created_at users]
+    end
+
+    private_class_method :full_parameters
   end
 end
