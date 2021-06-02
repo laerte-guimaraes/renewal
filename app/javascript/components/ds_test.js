c210c46c38891428a0c512e94e471f59f720c081
laerte-guimaraes <laerte.guimaraes@vindi.com.br>
2021-06-02 19:15:08 -0300

    WIP - Tentativa de exibir componente na view

---
+++ app/javascript/components/ds_test.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Theme, Banner } from 'vindi-ds-components'
+
+function render(){
+  return (
+    <Theme>
+      <Banner withClose>
+        "Use banners para a comunicação da Vindi na plataforma. Consulte o guia de Tom & Voz na hora de escrever :)"
+      </Banner>
+    </Theme>
+  )
+}
