c7496eeb4ecc95ce9f1de0b9fff0bb8616fa22e0
laerte-guimaraes <laerte.guimaraes@vindi.com.br>
2021-06-02 19:36:47 -0300

    Insere export para função criada

--- app/javascript/components/ds_test.js
+++ app/javascript/components/ds_test.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react'
 import { Theme, Banner } from 'vindi-ds-components'
 
-function render(){
+export default function banner(){
   return (
     <Theme>
       <Banner withClose>
